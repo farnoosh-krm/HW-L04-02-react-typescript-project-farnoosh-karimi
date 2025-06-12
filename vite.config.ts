4d84062f3a7ba6cf5dc57dd6cf353dd6cd0e8fb7
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-12 11:29:26 +0330

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+  preview: {
+    host: '0.0.0.0', // برای دسترسی از خارج
+    port: 10000,
+    allowedHosts: [
+      'hw-l04-02-react-typescript-project-pgw5.onrender.com', 
+    ],
+  },
+});
